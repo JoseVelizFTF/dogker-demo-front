4d6982b33a717b95d4e13853d67268db8059d635
JoseVelizFTF <jose.veliz@fulltimeforce.com>
2020-08-02 11:32:28 -0500

    added print errors

---
+++ src/Logger.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import { getSocket } from "./api/socket";
+export const Logger = () => {
+  const socket = getSocket();
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const setIntervalId = setInterval(() => {
+      messages.length > 0 &&
+        setMessages(messages.splice(0, messages.length - 1));
+    }, 3000);
+    return () => clearInterval(setIntervalId);
+  });
+
+  useEffect(() => {
+    socket &&
+      socket.on("err", (msg) => {
+        setMessages((msgs) => [...msgs, msg]);
+      });
+  }, [socket]);
+
+  return (
+    <div>
+      <ul style={{ textDecoration: "none", padding: 0, margin: 0 }}>
+        {messages.reverse().map((message, idx) => (
+          <li
+            style={{
+              backgroundColor: "red",
+              color: "white",
+              margin: "0.5rem",
+              borderRadius: "10px",
+              padding: "8px 0.5rem",
+            }}
+            key={idx}
+          >
+            <b>Error:</b> {message}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
